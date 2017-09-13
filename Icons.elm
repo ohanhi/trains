844390a0caf5b6b0a8fd10122c8f65c833be6599
ohanhi <ossi.hanhinen@gmail.com>
2017-09-13 18:45:37 +0300

    Use SVG for swap icon

---
+++ Icons.elm
@@ -0,0 +1,30 @@
+module Icons exposing (..)
+
+import Html exposing (Html)
+import Svg
+import Svg.Attributes exposing (..)
+
+
+swap : Float -> Html msg
+swap floatSize =
+    let
+        size =
+            toString (round floatSize)
+    in
+    Svg.svg
+        [ viewBox "0 0 13 13"
+        , width size
+        , height size
+        ]
+        [ Svg.path
+            [ strokeWidth "0.7"
+            , strokeLinecap "round"
+            , stroke "#babdb6"
+            , fill "none"
+            , d
+                ("M4 2 v10 M2 4 L4 2 M6 4 L4 2"
+                    ++ "M8 12 v-10 M6 10 L8 12 M10 10 L8 12"
+                )
+            ]
+            []
+        ]
