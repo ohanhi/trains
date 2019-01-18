ecc6eb2d45131672751b12e5a8d0c175fd4a6933
ohanhi <ossi.hanhinen@gmail.com>
2019-01-18 09:44:40 +0200

    Add more route tests

--- tests/Routing.elm
+++ tests/Routing.elm
@@ -10,16 +10,31 @@ import Url
 
 suite : Test
 suite =
-    test "Schedule page routes" <|
-        \_ ->
-            let
-                url =
-                    { protocol = Url.Https
-                    , host = ""
-                    , port_ = Nothing
-                    , path = "/"
-                    , query = Nothing
-                    , fragment = Just "HKI/M%C3%84K"
-                    }
-            in
-            Expect.equal (ScheduleRoute "HKI" "MÄK") (Main.parseUrl url)
+    Test.concat
+        [ test "Root route" <|
+            \_ ->
+                Expect.equal
+                    SelectDepRoute
+                    (Main.parseUrl <| urlWithFragment "/")
+        , test "Destination page route" <|
+            \_ ->
+                Expect.equal
+                    (SelectDestRoute "MÄK")
+                    (Main.parseUrl <| urlWithFragment "/M%C3%84K")
+        , test "Schedule page route" <|
+            \_ ->
+                Expect.equal
+                    (ScheduleRoute "HKI" "MÄK")
+                    (Main.parseUrl <| urlWithFragment "HKI/M%C3%84K")
+        ]
+
+
+urlWithFragment : String -> Url.Url
+urlWithFragment fragment =
+    { protocol = Url.Https
+    , host = ""
+    , port_ = Nothing
+    , path = "/"
+    , query = Nothing
+    , fragment = Just fragment
+    }
